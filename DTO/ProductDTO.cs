f66219c084371f46154bdf9c89efcb422e043e15
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-10-09 22:58:59 +0800

    Created Product DTO for data container.

---
+++ DTO/ProductDTO.cs
@@ -0,0 +1,14 @@
+namespace Backend.DTO;
+
+public class ProductDTO
+{
+    public string ProductName { get; set; }
+    public string ProductDescription { get; set; }
+    public float ProductPrice { get; set; }
+    // public float DiscountPrice { get; set; }
+    public int Stock { get; set; }
+    public int CategoryId { get; set; }
+    public int BrandId { get; set; }
+    public string? CategoryName { get; set; }
+    public string? BrandName { get; set; }
+}
