7d142b7ffe62374c0b72d2c61ba3c384f654d12a
Francis77v <francis.flores@valens-research.com>
2025-10-13 11:08:43 +0800

    Created a new ProductDTO for add function.

---
+++ DTO/ProductAddDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.DTO;
+
+public class ProductAddDTO
+{
+    public string ProductName { get; set; }
+    public string ProductDescription { get; set; }
+    public float ProductPrice { get; set; }
+    // public float DiscountPrice { get; set; }
+    public int Stock { get; set; }
+    public int CategoryId { get; set; }
+    public int BrandId { get; set; }
+}
