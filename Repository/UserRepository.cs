4d2eee5febce5ca3dcbacedbf71026909f7c63fd
Francis77v <floresfrancisjoseph28@gmail.com>
2025-10-15 15:42:29 +0800

    Create a repository for user CRUD and DI context.

---
+++ Repository/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace Backend.Repository;
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+using Backend.DTO;
+using Backend.Models;
+public class UserRepository
+{
+    private readonly EntityDbContext _context;
+
+    public UserRepository(EntityDbContext context)
+    {
+        _context = context;
+    }
+
+    // public Task<string> AddUser(UserCreateDTO u)
+    // {
+    //     var user = new Users
+    //     {
+    //         // UserName = u.Username,
+    //         // PasswordHash = 
+    //     };
+    //     return await "hash";
+    // }
+}
