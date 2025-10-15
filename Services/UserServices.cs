47c18836fb9123846aa72a79697419fb39d3b308
Francis77v <floresfrancisjoseph28@gmail.com>
2025-10-15 15:43:42 +0800

    Create user services

---
+++ Services/UserServices.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend.Services;
+using Backend.Models;
+using Backend.Repository;
+using Backend.DTO;
+public class UserServices
+{
+    private readonly UserRepository _repository;
+
+    public UserServices(UserRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<string> AddUserService(UserCreateDTO user)
+    {
+        var addUser = await _repository.AddUser(user);
+        return "asdsd";
+    }
+    
+}
