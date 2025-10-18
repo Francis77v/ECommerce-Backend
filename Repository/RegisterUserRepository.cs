ed334d7d5d09e27ab7a11f2ebf604309ac092f0c
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-10-18 20:34:37 +0800

    Rename class and add auto assign role function.

--- Repository/RegisterUserRepository.cs
+++ Repository/RegisterUserRepository.cs
@@ -4,44 +4,41 @@ using Microsoft.AspNetCore.Identity;
 using backend.Data;
 using Backend.DTO;
 using Backend.Models;
-public class UserRepository
+public class RegisterUserRepository
 {
     private readonly EntityDbContext _context;
     private readonly UserManager<Users> _userManager;
     private readonly RoleManager<IdentityRole> _roleManager;
 
-    public UserRepository(EntityDbContext context, UserManager<Users> userManager, RoleManager<IdentityRole> roleManager)
+    public RegisterUserRepository(EntityDbContext context, UserManager<Users> userManager, RoleManager<IdentityRole> roleManager)
     {
         _context = context;
         _userManager = userManager;
         _roleManager = roleManager;
     }
 
-    public async Task<string> AddUser(UserCreateDTO u)
+    //register user method
+    public async Task<string> RegisterUser(RegisterDTO u)
     {
         var user = new Users
         {
             UserName = u.Username,
             Email = u.Email
         };
-
- 
+        
         var result = await _userManager.CreateAsync(user, u.Password);
 
         if (!result.Succeeded)
             return string.Join(", ", result.Errors.Select(e => e.Description));
 
-
-        if (!string.IsNullOrEmpty(u.Role))
+        string roleName = "Customer";
+        if (!await _roleManager.RoleExistsAsync(roleName))
         {
-            if (!await _roleManager.RoleExistsAsync(u.Role))
-            {
-                await _roleManager.CreateAsync(new IdentityRole(u.Role));
-            }
-
-            await _userManager.AddToRoleAsync(user, u.Role);
+            await _roleManager.CreateAsync(new IdentityRole(roleName));
         }
-        return $"User {user.UserName} created successfully with role {u.Role}!";
+        
+        await _userManager.AddToRoleAsync(user, roleName);
+        
+        return $"User {user.UserName} created successfully!";
     }
-
 }
