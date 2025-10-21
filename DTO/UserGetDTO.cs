1ece84829ae4cb8e4ede5c937f9da600b37482be
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2025-10-21 22:26:14 +0800

    Add Id property.

--- DTO/UserGetDTO.cs
+++ DTO/UserGetDTO.cs
@@ -2,6 +2,7 @@ namespace Backend.DTO;
 
 public class UserGetDTO
 {
+    public string Id { get; set; }
     public string UserName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public bool EmailConfirmed { get; set; }
